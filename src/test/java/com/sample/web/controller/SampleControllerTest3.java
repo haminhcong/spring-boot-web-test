90440288f49f715529a1c54b62a666d07200ec0c
haminhcong <haminhcongbkhn@gmail.com>
2018-08-29 00:45:08 +0700

    add test about auto wire and spy bean in mockMvc Spring Boot Test

---
+++ src/test/java/com/sample/web/controller/SampleControllerTest3.java
@@ -0,0 +1,89 @@
+package com.sample.web.controller;
+
+import com.sample.web.dto.ClientDataDTO;
+import com.sample.web.dto.ClientHealthStatusInfo;
+import com.sample.web.dto.ClientIm2InfoDTO;
+import com.sample.web.dto.HealthInfoDTO;
+import com.sample.web.service.DataSimulateService;
+import com.sample.web.service.HttpClientSimulateService;
+import com.sample.web.utils.DataUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// Test failed. Demonstrate that Mock Bean override Autowire Bean
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+public class SampleControllerTest3 {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    // We can use Autowire or SpyBean
+    @SpyBean
+    private DataSimulateService dataSimulateService;
+
+    @MockBean
+    private HttpClientSimulateService httpClientSimulateService;
+
+    private Long clientId = 3L;
+    private ClientDataDTO clientWithId3Fake = new ClientDataDTO(
+            3L,
+            "customer3",
+            "yenhoa-hanoi",
+            "customer3@email.com",
+            "{'height':175, 'weight':80}");
+
+    // correct equals Object
+    private ClientIm2InfoDTO im2InfoDTOFakeCorrect = new ClientIm2InfoDTO(
+            3L,
+            "customer3",
+            "yenhoa",
+            "hanoi",
+            "customer3@email.com",
+            new HealthInfoDTO(175L, 80L)
+    );
+    // incorrect  not equals Object
+    private ClientIm2InfoDTO im2InfoDTOFakeInCorrect = new ClientIm2InfoDTO(
+            4L,
+            "customer4",
+            "yenhoa1234",
+            "hanoi",
+            "customer3@email.com",
+            new HealthInfoDTO(175L, 80L)
+    );
+
+    private ClientHealthStatusInfo healthStatusInfoFakeOutput =
+            new ClientHealthStatusInfo(
+                    3L,
+                    "customer3",
+                    "yenhoa",
+                    "hanoi",
+                    "customer3@email.com",
+                    "good"
+            );
+
+    @Test
+    public void getGoodClientStatusInfoPassTest() throws Exception {
+        this.mockMvc
+                .perform(get("/data/client")
+                        .param("id", clientId.toString())
+                        .accept(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                ).andExpect(status().isOk())
+                .andExpect(content().json(
+                        DataUtils.convertObjectToJsonString(healthStatusInfoFakeOutput)));
+    }
+}
+
